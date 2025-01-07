eedb54c7434804099e6ed0dd09d9370f800087f1
Abdulaziz20007 <lil.gangsser@gmail.com>
2025-01-07 15:48:35 +0500

    otp

--- controllers/otp.controller.js
+++ controllers/otp.controller.js
@@ -3,7 +3,7 @@ const otpGenerator = require("otp-generator");
 const { v4: uuidv4 } = require("uuid");
 const { errorHandler } = require("../helpers/error_handler");
 const config = require("config");
-const { encode } = require("../services/crypt");
+const { encode, decode } = require("../services/crypt");
 const addMinutesToDate = require("../helpers/add_minutes");
 
 const createOtp = async (req, res) => {
@@ -36,6 +36,64 @@ const createOtp = async (req, res) => {
   }
 };
 
+const verifyOtpClient = async (req, res) => {
+  try {
+    const { verification_key, phone_number, otp } = req.body;
+    const currentTime = new Date();
+
+    const decodedData = await decode(verification_key);
+    const parsedData = JSON.parse(decodedData);
+    if (parsedData.phone_number !== phone_number) {
+      const response = {
+        Status: "Failed",
+        Details: "OTP was not sent to this particular phone number",
+      };
+      return res.status(400).send(response);
+    }
+    const otpResult = await pool.query(`SELECT * FROM otp WHERE id = $1`, [
+      parsedData.otp_id,
+    ]);
+    const result = otpResult.rows[0];
+
+    if (result == null) {
+      const response = {
+        Status: "Failed",
+        Details: "OTP not found",
+      };
+      return res.status(400).send(response);
+    }
+
+    if (result.verified == true) {
+      const response = {
+        Status: "Failed",
+        Details: "OTP already verified",
+      };
+      return res.status(400).send(response);
+    }
+
+    if (result.expiration_time < currentTime) {
+      const response = {
+        Status: "Failed",
+        Details: "OTP expired",
+      };
+      return res.status(400).send(response);
+    }
+
+    if (result.otp !== otp) {
+      const response = {
+        Status: "Failed",
+        Details: "OTP not matched",
+      };
+      return res.status(400).send(response);
+    }
+
+    // res.send(decodedData);
+  } catch (err) {
+    errorHandler(err, res);
+  }
+};
+
 module.exports = {
   createOtp,
+  verifyOtpClient,
 };
--- routes/otp.routes.js
+++ routes/otp.routes.js
@@ -1,7 +1,8 @@
-const { createOtp } = require("../controllers/otp.controller");
+const { createOtp, verifyOtpClient } = require("../controllers/otp.controller");
 
 const router = require("express").Router();
 
 router.post("/", createOtp);
+router.post("/verifyclient", verifyOtpClient);
 
 module.exports = router;
